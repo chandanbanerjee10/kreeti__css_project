20191d984e863bfb0a6442dbeaf2de44182a4db8
chandan_banerjee <chandanbanerjee514@gmail.com>
2022-04-08 10:23:15 +0530

    hamburger using css

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 
-    function myFunction(){
-        let info = document.getElementById("info");
-        let showmenu = document.querySelector('nav ul');
-        var x = window.matchMedia("(max-width: 500px)");
-        if(x.matches){
-            if(showmenu.style.display === "block"){
-                showmenu.style.display = "none";
-                info.style.display = "block";
-            }else{
-                showmenu.style.display = "block";
-                info.style.display = "none";
-            }
-        }
-    }
+    // function myFunction(){
+    //     let info = document.getElementById("info");
+    //     let showmenu = document.querySelector('nav ul');
+    //     var x = window.matchMedia("(max-width: 500px)");
+    //     if(x.matches){
+    //         if(showmenu.style.display === "block"){
+    //             showmenu.style.display = "none";
+    //             info.style.display = "block";
+    //         }else{
+    //             showmenu.style.display = "block";
+    //             info.style.display = "none";
+    //         }
+    //     }
+    // }
 
+    // function myFunction() {
+    //     var x = document.getElementsByClassName("header__second__list--icons")[0];
+    //     console.log(x)
+    //     if (x.className === "header__second__list--icons") {
+    //       x.className += " responsive";
+    //     } else {
+    //       x.className = "header__second__list--icons";
+    //     }
+    //   }
 
-    var mq = window.matchMedia( "(max-width: 570px)" );
-    if (!mq.matches) {
-        var x = document.getElementsByClassName('header__second__list--icons');
-        x
-    }
-    else {
-        // window width is greater than 570px
-    }
-    
 
